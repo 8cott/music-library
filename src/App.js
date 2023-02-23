386d81eacea66bf6169763a63265cfe6e7b82a47
8cott <scottrubin@gmail.com>
2023-02-22 19:14:23 -0500

    data context

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import Gallery from './components/Gallery';
 import SearchBar from './components/SearchBar';
+import { DataContext } from './context/DataContext';
 import './App.css';
 
 function App() {
@@ -35,7 +36,9 @@ const handleSearch = (e, term) => {
     <div className="App">
       <SearchBar handleSearch = {handleSearch} />
       {message}
-      <Gallery data={data} /> 
+      <DataContext.Provider value={data}>
+          <Gallery /> 
+      </DataContext.Provider>
     </div>
   );
 }
