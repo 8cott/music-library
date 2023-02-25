a2ed71aa64a2a12d9894f4a8c909e3f5afec11f6
8cott <scottrubin@gmail.com>
2023-02-25 12:00:52 -0500

    finished improve-a-music-library-interface

--- src/components/AlbumView.js
+++ src/components/AlbumView.js
@@ -1,16 +1,52 @@
-import { useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { useParams, useNavigate, Link } from 'react-router-dom'
 
 function AlbumView() {
+    const navigate = useNavigate()
     const { id } = useParams()
-    const [ albumData, setalbumData ] = useState([])
+    const [ albumData, setAlbumData ] = useState([])
+
+    useEffect(() => {
+        const API_URL = `http://localhost:4000/song/${id}`
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setAlbumData(resData.results)
+        }
+        fetchData()
+    }, [id])
+
+    const justSongs = albumData.filter(entry => entry.wrapperType === 'track')
+
+    const navButtons = () => {
+        return(
+            <div>
+                <button onClick={() => navigate(-1)}>Back</button>
+                |
+                <button onClick={() => navigate('/')}>Home</button>
+            </div>
+        )
+    }
+
+    const renderSongs = justSongs.map((song, i) => {
+        return (
+            <div key={i}>
+                <Link>
+                <p>{song.trackName}</p>
+                </Link>
+            </div>
+        )
+    })
 
     return (
         <div>
-            <h2>The id passed was: {id}</h2>
-            <p>Album Data Goes Here!</p>
+            {navButtons()}
+            {albumData.length > 0 ?<h2>{albumData[0].albumName}</h2> : <h2>Loading...</h2>}
+            {renderSongs}
         </div>
     )
+
 }
+
 
 export default AlbumView
